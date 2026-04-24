42ed58b836fd67db64edbbe91f3d9986f0a9b152
Lahcen Sebar <lsebar@c3r3p4.1337.ma>
2026-04-24 22:48:38 +0100

    up

--- parsing.c
+++ parsing.c
@@ -16,93 +16,108 @@
 #include <string.h>
 #include <unistd.h>
 
+int check_str_greather_intmax(const char *s)
+{
+	int		s_len;
+	int		intmax_len;
+	char	*intmax;
 
+	intmax = "2147483647";
+	intmax_len = strlen(intmax);
+	s_len = strlen(s);
 
-
+	if (s_len > intmax_len)
+		return 1;
+	else if (s_len < intmax_len)
+		return 0;
+	else
+		return strcmp(s, intmax) > 0;
+}
 
 
 int main(int argc, char **argv)
 {
-	int i;
-	int number_of_coders;
-	int time_to_burnout;
-	int time_to_compile;
-	int time_to_debug;
-	int time_to_refactor;
-	int number_of_compiles_required;
-	int dongle_cooldown;
-	char *scheduler;
+	int		i;
+	int		number_of_coders;
+	int		time_to_burnout;
+	int		time_to_compile;
+	int		time_to_debug;
+	int		time_to_refactor;
+	int		number_of_compiles_required;
+	int		dongle_cooldown;
+	char	*scheduler;
 
-	//if user gives nothing?
-	//if user gives 1 or 2 but the rest nothing?
-
-	if (atoi(argv[1]) && atoi(argv[1]) != 0)
-		number_of_coders = atoi(argv[1]);
-
-	else
+	if (argc == 9)
 	{
-		number_of_coders = 0;
-		printf("Invalid numbuer_of_coders");
+		if (atoi(argv[1]) && atoi(argv[1]) != 0 && !check_str_greather_intmax(argv[1]))
+			number_of_coders = atoi(argv[1]);
+
+		else
+		{
+			number_of_coders = 0;
+			printf("Invalid numbuer_of_coders");
+		}
+
+		if (atoi(argv[2]) && atoi(argv[2]) != 0 && !check_str_greather_intmax(argv[2]))
+			time_to_burnout = atoi(argv[2]);
+
+		else
+		{
+			time_to_burnout = 0;
+			printf("Invalid time_to_burnout");
+		}
+
+		if (atoi(argv[3]) && atoi(argv[3]) != 0 && !check_str_greather_intmax(argv[3]))
+			time_to_compile = atoi(argv[3]);
+
+		else
+		{
+			time_to_compile = 0;
+			printf("Invalid time_to_compile");
+		}
+
+		if (atoi(argv[4]) && atoi(argv[4]) != 0 && !check_str_greather_intmax(argv[4]))
+			time_to_debug = atoi(argv[4]);
+		
+		else
+		{
+			time_to_debug = 0;
+			printf("Invalid time_to_debug");
+		}
+
+		if (atoi(argv[5]) && atoi(argv[5]) != 0 && !check_str_greather_intmax(argv[5]))
+			time_to_refactor = atoi(argv[5]);
+
+		else
+		{
+			time_to_refactor = 0;
+			printf("Invalid time_to_refactor");
+		}
+
+		if (atoi(argv[6]) && atoi(argv[6]) != 0 && !check_str_greather_intmax(argv[6]))
+			number_of_compiles_required = atoi(argv[6]);
+
+		else
+		{
+			number_of_compiles_required = 0;
+			printf("Invalid number_of_compiles_required");
+		}
+
+		if (atoi(argv[7]) && atoi(argv[7]) != 0 && !check_str_greather_intmax(argv[7]))
+			dongle_cooldown = atoi(argv[7]);
+
+		else
+		{
+			dongle_cooldown = 0;
+			printf("Invalid dongle_cooldown");
+		}
+
+		if (!(strcmp(argv[8], "edf") && strcmp(argv[8], "fifo")))
+			scheduler = argv[8];
+		else
+			printf("Invalid scheduler");
 	}
-
-	if (atoi(argv[2]) && atoi(argv[2]) != 0)
-		time_to_burnout = atoi(argv[2]);
-	
 	else
-	{
-		time_to_burnout = 0;
-		printf("Invalid time_to_burnout");
-	}
-
-	if (atoi(argv[3]) && atoi(argv[3]) != 0)
-		time_to_compile = atoi(argv[3]);
-	
-	else
-	{
-		time_to_compile = 0;
-		printf("Invalid time_to_compile");
-	}
-
-	if (atoi(argv[4]) && atoi(argv[4]) != 0)
-		time_to_debug = atoi(argv[4]);
-	
-	else
-	{
-		time_to_debug = 0;
-		printf("Invalid time_to_debug");
-	}
-
-	if (atoi(argv[5]) && atoi(argv[5]) != 0)
-		time_to_refactor = atoi(argv[5]);
-	
-	else
-	{
-		time_to_refactor = 0;
-		printf("Invalid time_to_refactor");
-	}
-
-	if (atoi(argv[6]) && atoi(argv[6]) != 0)
-		number_of_compiles_required = atoi(argv[6]);
-	
-	else
-	{
-		number_of_compiles_required = 0;
-		printf("Invalid number_of_compiles_required");
-	}
-
-	if (atoi(argv[7]) && atoi(argv[7]) != 0)
-		dongle_cooldown = atoi(argv[7]);
-	
-	else
-	{
-		dongle_cooldown = 0;
-		printf("Invalid dongle_cooldown");
-	}
-
-	if (strcmp(argv[8], "edf") || strcmp(argv[8], "fifo"))
-		scheduler = argv[8];
-	else
-		printf("Invalid scheduler");
-
+		printf("The arguments have to be exactly 9 args");
 }
 
