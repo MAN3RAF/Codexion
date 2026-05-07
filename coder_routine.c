d243e18d620191c71f160c2ab183dcd51a699aa2
Lahcen Sebar <lsebar@c3r3p4.1337.ma>
2026-05-07 20:58:15 +0100

    up

--- coder_routine.c
+++ coder_routine.c
@@ -35,31 +35,18 @@ void	*coder_routine(void *arg)
     coder = (t_coder *)arg;
     pthread_mutex_lock(&coder->system->start_lock);
     while (!coder->system->all_threads_ready)
-        pthread_cond_wait(&coder->system->start_line, &coder->system->start_lock);
+        pthread_cond_wait(&coder->system->start_line,&coder->system->start_lock);
     pthread_mutex_unlock(&coder->system->start_lock);
-
-    // the idea here is to make all the threads wait for others to group up and enter together!
-    // if (DEBUGGING == 1)
-    //     write(1, "Good!\n", 6);
     time = get_time_ms() - coder->system->start_time_ms;
     if (coder->id % 2 == 0)
         safe_sleep(coder->system->number_of_compiles_required);
-
     while(1)
     {
-        if (DEBUGGING == 1)
-            write(1, "Good before!\n", 13);
         if (is_simulation_end(coder))
             break;
-        if (DEBUGGING == 1)
-            write(1, "Good after!\n", 13);
         if (is_compiled_enough(coder))
             break;
-        if (DEBUGGING == 1)
-            write(1, "Good after2!\n", 14);
         hold_dongle(coder, coder->first);
-        if (DEBUGGING == 1)
-            write(1, "Good dongle!\n", 13);
         hold_dongle(coder, coder->second);
         compile_phase(coder);
     }
